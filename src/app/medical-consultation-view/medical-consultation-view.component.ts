004adb042ec5411e8a9e4e907a151969905740ae
Sebastian Jordan <sebastianjmt@hotmail.com>
2018-05-08 23:36:56 -0400

    refactoring code

--- src/app/medical-consultation-view/medical-consultation-view.component.ts
+++ src/app/medical-consultation-view/medical-consultation-view.component.ts
@@ -12,14 +12,16 @@ export class MedicalConsultationViewComponent implements OnInit {
   private consultation:any;
   private id: string;
 
-  constructor(private httpService: HttpService,private route: ActivatedRoute, private location: Location) { }
+  // constructor(private httpService: HttpService,private route: ActivatedRoute, private location: Location) { }
+  constructor(private route: ActivatedRoute, private location: Location) { }
 
   ngOnInit() {
       this.id = this.route.snapshot.paramMap.get('id');
       this.consultation = {doctor: "",description:"",diagnostic:"",hospital:"",commentary:""};
-      this.httpService.get('consultation/get?id='+this.id).subscribe((response: any) => {
-          this.consultation = response.response;
-      })
+      // this.httpService.get('consultation/get?id='+this.id).subscribe((response: any) => {
+      //   if(response.success)
+      //     this.consultation = response.response;
+      // })
   }
   goBack() {
     this.location.back();
