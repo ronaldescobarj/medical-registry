729aa9f06849b3410123e6d6482a8798f6b38b60
Sebastian Jordan <sebastianjmt@hotmail.com>
2018-05-10 19:31:42 -0400

    format changes

--- src/app/medical-consultation-view/medical-consultation-view.component.ts
+++ src/app/medical-consultation-view/medical-consultation-view.component.ts
@@ -10,18 +10,19 @@ import { HttpService } from '../http.service';
 })
 export class MedicalConsultationViewComponent implements OnInit {
 
-  private consultation:any;
+  private consultation: any;
   private id: string;
 
   constructor(private httpService: HttpService, private route: ActivatedRoute, private location: Location) { }
 
   ngOnInit() {
-      this.id = this.route.snapshot.paramMap.get('id');
-      // this.consultation = {doctor: "",description:"",diagnostic:"",hospital:"",commentary:""};
-      this.httpService.get('/consultation/get?id='+this.id).subscribe((response: any) => {
-          this.consultation = response.response[0];
-      })
+    this.id = this.route.snapshot.paramMap.get('id');
+    // this.consultation = {doctor: "",description:"",diagnostic:"",hospital:"",commentary:""};
+    this.httpService.get('/consultation/get?id=' + this.id).subscribe((response: any) => {
+      this.consultation = response.response[0];
+    })
   }
+
   goBack() {
     this.location.back();
   }
